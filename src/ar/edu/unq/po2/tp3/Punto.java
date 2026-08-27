97e2fbbea5f274ad629e28fec994bed17f3399c2
Todini Oriana <todini17@gmail.com>
2026-08-27 18:01:21 -0300

    versin inicial

---
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto {
+   private Float x;
+   private Float y;
+   public Punto(Float x, Float y) {
+	super();
+	this.x = x;
+	this.y = y;
+   }
+   public Float getX() {
+	return x;
+   }
+   public void setX(Float x) {
+	this.x = x;
+   }
+   public Float getY() {
+	return y;
+   }
+   public void setY(Float y) {
+	this.y = y;
+   }
+   
+   
+}
